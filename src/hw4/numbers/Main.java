1876001143a1b9aff7d171a8dca46fc5fc1eb6cf
MaxKhromov <sloopemail@gmail.com>
2022-12-19 23:06:09 +0400

    hw4.numbers (FizzBuzz) lambda usage added

--- src/hw4/numbers/Main.java
+++ src/hw4/numbers/Main.java
@@ -1,8 +1,13 @@
 package hw4.numbers;
 
 import java.util.Objects;
+import java.util.stream.IntStream;
 
 public class Main {
+    interface FizzBuzz {
+        String fizzBuzzing();
+    }
+
     public static void main(String[] args) {
         //Напишите программу, которая выводит на экран числа от 1 до 100. При этом вместо чисел, кратных трем,
         //
@@ -11,32 +16,26 @@ public class Main {
         //Если число кратно пятнадцати, то программа должна выводить слово FizzBuzz.
         //
         //Задача может показаться очевидной, но нужно получить наиболее простое и красивое решение.
+
+        IntStream
+                .rangeClosed(1, 100)
+                .forEach(i -> System.out
+                        .println(i % 3 == 0 ? (i % 5 == 0 ? "FizzBuzz" : "Fizz") : (i % 5 == 0 ? "Buzz" : i)));
+
+        IntStream
+                .rangeClosed(1, 100)
+                .forEach(i -> fizzBuzz(() -> i % 3 == 0 ? (i % 5 == 0 ? "FizzBuzz" : "Fizz") : (i % 5 == 0 ? "Buzz" : Objects.toString(i))));
+
+
         for (int i = 1; i <= 100; i++) {
-            String output = "" + (i % 3 == 0 ? "Fizz" : "") + (i % 5 == 0 ? "Buzz" : "");
-            System.out.println(output.isEmpty() ? Objects.toString(i) : output);
-
-//            if(i%3 == 0 && i%5 == 0){
-//                System.out.println("FizzBuzz");
-//            } else if (i%3==0){
-//                System.out.println("Fizz");
-//            }else if(i%5==0){
-//                System.out.println("Buzz");
-//            } else {
-//                System.out.println(i);
-//            }
-
-//            String output = "";
-//            if(i%3==0){
-//                output = output + "Fizz";
-//            }
-//            if(i%5==0){
-//                output = output + "Buzz";
-//            }
-//            if(output.isEmpty()){
-//                output = output + Objects.toString(i);
-//            }
-//            System.out.println(output);
-
+            int finalI = i;
+            fizzBuzz(() ->
+                    finalI % 3 == 0 ? (finalI % 5 == 0 ? "FizzBuzz" : "Fizz") : (finalI % 5 == 0 ? "Buzz" : Objects.toString(finalI))
+            );
         }
+    }
+
+    public static void fizzBuzz(FizzBuzz buzzFisting) {
+        System.out.println(buzzFisting.fizzBuzzing());
     }
 }
